4ea17ac44a41b0dc763d4189c66463c9cf6475e5
rashgaroth <50513263+rashgaroth@users.noreply.github.com>
2023-05-27 19:27:15 +0700

    feat: ci remove affected

--- packages/image-gen/src/lib/image-gen.ts
+++ packages/image-gen/src/lib/image-gen.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { readFile } from 'fs/promises'
-import * as fs from 'fs'
+import { readFile } from 'fs/promises';
+import * as fs from 'fs';
 import * as sharp from 'sharp';
 import * as path from 'path';
 import { sizes } from '../common/constants';
@@ -12,44 +12,47 @@ function joinCwd(...p: string[]): string {
 function isDir(p: string): boolean {
   try {
     const stat = fs.lstatSync(p);
-    return stat.isDirectory()
+    return stat.isDirectory();
   } catch (e) {
     return false;
   }
 }
 
-const isSlash = (p: string) => p.endsWith('/')
+const isSlash = (p: string) => p.endsWith('/');
 
 export const generateImages = async (inputPath: string, outputPath: string) => {
-  const iPath = joinCwd(`./`, inputPath)
-  const oPath = joinCwd(`./`, outputPath)
+  const iPath = joinCwd(`./`, inputPath);
+  const oPath = joinCwd(`./`, outputPath);
 
-  const img = await readFile(iPath)
+  const img = await readFile(iPath);
   const isOutDir = isDir(oPath);
 
   if (!isOutDir) {
-    throw new Error(`${oPath} is not directory`)
+    throw new Error(`${oPath} is not directory`);
   }
   if (!img) {
-    throw new Error(`File does not exist`)
+    throw new Error(`File does not exist`);
   }
 
   for (const size of sizes) {
-    const fName = size.height === 32 ? `${size.name}${size.ext}` : `${size.name}-${size.width}-${size.height}${size.ext}`
+    const fName =
+      size.height === 32
+        ? `${size.name}${size.ext}`
+        : `${size.name}-${size.width}-${size.height}${size.ext}`;
     sharp(iPath)
       .resize({
         width: size.width,
         height: size.height,
         withoutEnlargement: true,
-        fit: sharp.fit.inside
+        fit: sharp.fit.inside,
       })
       .toFile(`${oPath}${isSlash(oPath) ? fName : `/${fName}`}`, (err: any) => {
         if (err) {
-          throw new Error(err)
+          throw new Error(err);
         }
         console.log(`
 🚀 Generating ${fName}
-        `)
-      })
+        `);
+      });
   }
-}
+};
